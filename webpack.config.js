ae26342e62daa528d724a4e73e0dce6b3ded8bc8
Alex Chon <achon@uwaterloo.ca>
2024-07-15 19:39:59 +0900

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/appendFile.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/, 
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin ({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html'
+        })
+    ]
+}
