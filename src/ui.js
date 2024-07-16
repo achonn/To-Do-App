45fd3de08e86e1ef6f4e0e65ab68cc52357d7689
Alex Chon <achon@uwaterloo.ca>
2024-07-16 10:24:15 +0900

    Append Tasks Function Commit

--- src/ui.js
+++ src/ui.js
@@ -1,16 +1,11 @@
 import taskManager from "./tasksManager";
 
-const inputTodo = document.querySelector('.inputToDo').value;
-const taskList = document.querySelector('.taskList');
-const appendButton = document.querySelector('#appendNewTaskButton');
-
-
-
 const DOMManipulator = {
     renderToDos() {
         const toDos = taskManager.getToDos();
         const taskList = document.querySelector('.taskList');
         taskList.innerHTML = '';
+
         toDos.forEach((toDo, index) => {
             const listItem = document.createElement('li');
             listItem.innerHTML = `
@@ -18,20 +13,30 @@ const DOMManipulator = {
                 <input type="checkbox" class="checkbox" ${toDo.completed ? "checked" : ""}>
                 <p>${toDo.toDoTitle}</p>
                 <div class="icons">
-                    <i class="fa-regular fa-pen-to-square"></i>
-                    <i class="fa-solid fa-trash"></i>
+                    <i class="fa-regular fa-pen-to-square" "edit"></i>
+                    <i class="fa-solid fa-trash" "trash"></i>
                 </div>
             </div>`
+
+            function deleteToDos() {
+                const deleteButton = document.querySelector('.trash');
+                deleteButton.addEventListener('click', function() {
+                    taskManager.deleteToDo(index);
+                    this.renderToDos();
+                })
+            };
         })
     },
 
     appendToDos() {
-
+        const toDoTitle = document.querySelector('.inputToDo').value;
+        const appendButton = document.querySelector('#appendNewTaskButton');
+        appendButton.addEventListener('click', function() {
+            taskManager.addToDo(toDoTitle);
+            this.renderToDos();
+        })
     },
 
-    deleteToDos() {
-
-    },
 
     updateProgressBar() {
         // in relation to the number of completed tasks / number of total tasks
