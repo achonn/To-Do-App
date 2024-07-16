e1c7f75b2546a157268901f31925c55ee954910c
Alex Chon <achon@uwaterloo.ca>
2024-07-16 12:41:14 +0900

    Confetti Commit

--- src/ui.js
+++ src/ui.js
@@ -70,6 +70,52 @@ const DOMManipulator = (function(){
         progressBar.style.width = `${progress}%`;
 
         document.getElementById('numbers').innerText = `${completedTasks / totalTasks}`;
+
+        if (toDos.length && completedTasks == totalTasks) {
+            blastConfetti();
+        }
+    };
+
+    const blastConfetti = function() {
+        const count = 200,
+        defaults = {
+          origin: { y: 0.7 },
+        };
+      
+      function fire(particleRatio, opts) {
+        confetti(
+          Object.assign({}, defaults, opts, {
+            particleCount: Math.floor(count * particleRatio),
+          })
+        );
+      }
+      
+      fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+      });
+      
+      fire(0.2, {
+        spread: 60,
+      });
+      
+      fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8,
+      });
+      
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2,
+      });
+      
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+      });
     }
 
     const initialize = function() {
